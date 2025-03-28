1ffd6c417c08ba8db51d9dd198ec99977b251d28
kirti berekar <berekarkirti11@gmail.com>
2025-03-28 17:03:56 +0530

    para done

--- Next.js Work/project structure/project/src/components/home/Home1.jsx
+++ Next.js Work/project structure/project/src/components/home/Home1.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Home1 = () => {
   return (
     <div>
-       <h1 className='min-h-screen bg-pink-400'>Home-1</h1>
+       <h1 className='min-h-screen bg-pink-400'>Home-1 </h1>
+       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti dicta distinctio soluta aliquid eligendi! Ullam delectus enim veritatis odio magnam temporibus cupiditate dolorum reiciendis labore, soluta pariatur officiis quasi. Repudiandae.</p>
     </div>
   )
 }
